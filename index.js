f7f7aae50df30733ba4cac5d441b72ea971f16d4
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-01-26 18:27:06 -0800

    homework9

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 //Beginning varibles..........
 const fs = require("fs");
-const generateHTML = require("./generatehtml");
+const generateHTML = require("./generateHTML");
 const inquirer = require("inquirer");
 const axios = require("axios");
 var pdf = require('html-pdf');
+
+function writeToFile(fileName, htmlContent) {
+    fs.writeFile(fileName, htmlContent, function (err) {
+        if (err) {
+            console.log("Error writing file");
+        }
+        else {
+            console.log("Profile successfully generated");
+        }
+    })
+}
 
 //Questions array used to prompt user.........
 const questions = [
@@ -30,16 +41,17 @@ inquirer.prompt(questions).then(function({ username, color }) {
   const queryUrl = `https://api.github.com/users/${username}`;
 
   axios.get(queryUrl).then(function(response) {
-    console.log(response.data)
+    // console.log(response.data)
 
     /* IF we wanted to stay in the browser we would write to file. */
     // fs.writeFile("generat.html", generateHTML(response, color), function(err, result){
     //   if (err) console.log('error', err);
     // })
-
+    let htmlContent = generateHTML(response, color);
   //  Generating pdf and calling our generateHTML function pushing our axious response into the html file.....
-    pdf.create(generateHTML(response, color)).toFile('./profile.pdf', function(err, res) {
+    pdf.create(htmlContent).toFile('./profile.pdf', function(err, res) {
        if (err) return console.log(err);
+       writeToFile("profile.html",htmlContent);
         console.log(res); 
    });
 
